fe6a06defac7179252eedb00097108f61f690e42
wnykuang <kuangwinnie@gmail.com>
2019-04-20 11:41:55 -0400

    implement spade_high

--- example11.py
+++ example11.py
@@ -9,8 +9,16 @@ class FrenchDeck:
         self._cards = [Card(rank , suit) for suit in self.suits 
                                             for rank in self.ranks]
         
-        def __len__(self):
-            return len(self._cards)
+    def __len__(self):
+        return len(self._cards)
         
-        def __getitem__(self, position):
-            return self._cards[position]
+    def __getitem__(self, position):
+        return self._cards[position]
+    
+
+suit_values = dict(spades = 3, hearts = 2, diamonds = 1, clubs = 0)
+def spades_high(card):
+    rankVal = FrenchDeck.ranks.index(card.rank)
+    #.index return the index of card.rank in FrenchDeck.ranks
+    print(rankVal)
+    return rankVal * len(suit_values) + suit_values[card.suit]
